3db849cd0f44d18f65f4807d9e775ee059b074aa
Marcos Benedicto <marcosarg99@gmail.com>
2024-01-12 11:17:38 -0300

    cambio en el bash

--- baja-grafica.sh
+++ baja-grafica.sh
@@ -5,8 +5,8 @@ echo "Campos bajados"
 
 #### Grafica cada campo
 
-# python main.py --graf-850-titae-viento -l db/lista-fechas.csv assets/
-# python main.py --graf-500-hgt -l db/lista-fechas.csv assets/
-# python main.py --graf-pw -l db/lista-fechas.csv assets/
-# python main.py --graf-hgt-1000-ivt -l db/lista-fechas.csv assets/
-# echo "Ejecucion completa"
+python main.py --graf-850-titae-viento -l db/lista-fechas.csv assets/
+python main.py --graf-500-hgt -l db/lista-fechas.csv assets/
+python main.py --graf-pw -l db/lista-fechas.csv assets/
+python main.py --graf-hgt-1000-ivt -l db/lista-fechas.csv assets/
+echo "Ejecucion completa"
